533181747431a6c7c7b4041419665c1e264a60e8
Gabe <115848472+g-nbo@users.noreply.github.com>
2024-05-04 16:59:57 -0400

    Complete part 2

---
+++ script.js
@@ -0,0 +1,56 @@
+console.log('hi');
+
+// Part 1 of the lab
+
+let counter = 0;
+
+function incrementForever() {
+    counter++;
+    incrementForever();
+}
+
+try {
+    // incrementForever();
+} catch (err) {
+    console.log(err);
+    console.log(counter);
+}
+
+// This function will take a number and increment counter to that number
+
+function incrementTo(num) {
+    if (num <= 0 || num <= counter) return
+
+    // console.log(counter);
+
+    if (num === counter) return
+    counter += 1;
+
+    incrementTo(num);
+}
+
+try {
+    incrementTo(10);
+    console.log(counter);
+} catch (err) {
+    console.log(err);
+}
+
+let arr = [0, 2, [3, 4, [5, [6], [8]]]];
+const flattenArr = (arr) => {
+
+    if(arr.flat().length === arr.length) return arr.flat();
+    return () => flattenArr(arr.flat());
+};
+
+const trampoline = (f, ...args) => {
+    let result = f(...args);
+    while (typeof result === "function") {
+        result = result();
+    }
+    return result;
+};
+
+console.log(trampoline(flattenArr(arr)));
+
+// console.log(arr.flat().length );
